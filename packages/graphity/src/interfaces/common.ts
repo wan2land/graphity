fe76b4240ede596ce76594b31c61dd55576822cc
Changwan Jun <wan2land@gmail.com>
2020-11-17 19:19:06 +0900

    Link graphity to graphity/decorator

--- packages/graphity/src/interfaces/common.ts
+++
@@ -1,8 +0,0 @@
-
-export type ConstructType<T> = new (...args: any[]) => T
-
-export type MaybeArray<T> = T | T[]
-
-export type MaybePromise<T> = T | Promise<T>
-
-export type Callable = (...args: any) => any
--- packages/graphity/stubs/middlewares/nothing.ts
+++
@@ -1,7 +0,0 @@
-import { Middleware } from '../../src'
-
-export class Nothing implements Middleware {
-  public handle(_: {}, next: any) {
-    return next()
-  }
-}
