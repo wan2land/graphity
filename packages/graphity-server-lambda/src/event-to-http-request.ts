6ad6f3a3d04e4a71f30893a0665346813e83e2f6
Changwan Jun <wan2land@gmail.com>
2021-09-27 14:13:53 +0900

    fix aws-lambda

--- packages/graphity-server-lambda/src/event-to-http-request.ts
+++ packages/graphity-server-lambda/src/event-to-http-request.ts
@@ -2,8 +2,12 @@ import { APIGatewayProxyEvent, APIGatewayProxyEventV2 } from 'aws-lambda'
 import { HttpRequest } from 'graphity'
 
 export function eventToHttpRequest(event: APIGatewayProxyEvent | APIGatewayProxyEventV2): HttpRequest {
-  const headers = Object.entries(event.headers)
-    .reduce<Record<string, string>>((carry, [name, header]) => (carry[name.toLowerCase()] = header, carry), {}) // eslint-disable-line no-return-assign
+  const headers = Object.entries(event.headers).reduce<Record<string, string>>((carry, [name, header]) => {
+    if (header) {
+      carry[name.toLowerCase()] = header
+    }
+    return carry
+  }, {})
 
   return {
     host: event.requestContext.domainName ?? headers.host,
